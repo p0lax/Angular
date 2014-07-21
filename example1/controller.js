258d78d2f664df71908aaea43463afdc9a55db91
p0lax <polax@list.ru>
2014-07-21 19:15:08 +0400

    Data binding

--- example1/controller.js
+++ example1/controller.js
@@ -1,15 +1,8 @@
-function PhoneListCtrl($scope) {
-    $scope.phones = [
-        {"name": "Nexus S",
-            "snippet": "Fast just got faster with Nexus S.",
-            "age": 0},
-        {"name": "Motorola XOOM™ with Wi-Fi",
-            "snippet": "The Next, Next Generation tablet.",
-            "age": 1},
-        {"name": "MOTOROLA XOOM™",
-            "snippet": "The Next, Next Generation tablet.",
-            "age": 2}
-    ];
-
-    $scope.orderProp = 'age';
-}
+var phonecatApp = angular.module('phonecatApp', []);
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
+    function ($scope, $http) {debugger
+        $http.get('phones.json').success(function(data) {
+            $scope.phones = data;
+        });
+        $scope.orderProp = 'age';
+    }]);
