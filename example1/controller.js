e5124f1d4b2f1f0715d19a2f5c32eaf387d54ccf
p0lax <polax@list.ru>
2014-07-27 21:55:16 +0400

    src and id generating

--- example1/controller.js
+++ example1/controller.js
@@ -1,8 +1,19 @@
-var phonecatApp = angular.module('phonecatApp', []);
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
+'use strict';
+
+/* Controllers */
+
+var phonecatControllers = angular.module('phonecatControllers', []);
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
     function ($scope, $http) {
-        $http.get('phones.json').success(function(data) {
+        $http.get('phones/phones.json').success(function (data) {
             $scope.phones = data;
         });
+
         $scope.orderProp = 'age';
+    }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+    function ($scope, $routeParams) {
+        $scope.phoneId = $routeParams.phoneId;
     }]);
