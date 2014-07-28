92922d140326cf12c7b47c3b419b398f67bce250
p0lax <polax@list.ru>
2014-07-28 22:22:45 +0400

    src and id generating

--- example1/controller.js
+++ example1/controller.js
@@ -6,7 +6,7 @@ var phonecatControllers = angular.module('phonecatControllers', []);
 
 phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
     function ($scope, $http) {
-        $http.get('phones/phones.json').success(function (data) {
+        $http.get('phones.json').success(function (data) {
             $scope.phones = data;
         });
 
