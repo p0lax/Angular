4c6cc57a9a0702e342b3bb039fbd6f16d1dbd068
p0lax <polax@list.ru>
2014-08-07 13:33:02 +0400

    src and id generating

--- example1/app.js
+++ example1/app.js
@@ -1,21 +1,23 @@
 'use strict';
+
 /* App Module */
 
 var phonecatApp = angular.module('phonecatApp', [
     'ngRoute',
     'phonecatControllers',
-    'phonecatFilters'
+    'phonecatFilters',
+    'phonecatServices'
 ]);
 
 phonecatApp.config(['$routeProvider',
-    function ($routeProvider) {
+    function($routeProvider) {
         $routeProvider.
             when('/phones', {
-                templateUrl: 'views/phone-list.html',
+                templateUrl: 'partials/phone-list.html',
                 controller: 'PhoneListCtrl'
             }).
             when('/phones/:phoneId', {
-                templateUrl: 'views/phone-detail.html',
+                templateUrl: 'partials/phone-detail.html',
                 controller: 'PhoneDetailCtrl'
             }).
             otherwise({
--- example1/services.js
+++ example1/services.js
@@ -1,6 +1,12 @@
-angular.module('phonecatServices', ['ngResource']).
-    factory('Phone', function($resource){
+'use strict';
+
+/* Services */
+
+var phonecatServices = angular.module('phonecatServices', ['ngResource']);
+
+phonecatServices.factory('Phone', ['$resource',
+    function($resource){
         return $resource('phones/:phoneId.json', {}, {
             query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
         });
-    });
+    }]);
