b6f28105a188d8fa79808bd20f07c0231332b025
p0lax <andrey.rozhkov@dataart.com>
2018-02-27 11:26:22 +0100

    Initialization of the course App

--- example2/js/controllers.js
+++
@@ -1,6 +0,0 @@
-var certControllers = angular.module('certControllers', []);
-
-certControllers.controller('CertListController', ['$scope', 'certList',
-    function($scope, certList) {
-        $scope.certs = certList.query();
-    }]);
