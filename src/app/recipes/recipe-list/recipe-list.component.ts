41d8f795b19023c6d9ce09582968b4e0fdea4d40
p0lax <andrey.rozhkov@dataart.com>
2018-03-02 13:33:58 +0100

    Events binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { findIndex } from 'lodash';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,11 +10,17 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Recipe 1', 'This is a recipe!', 'https://cdn.pixabay.com/photo/2017/10/20/17/45/goulash-2872112_960_720.jpg'),
-    new Recipe('Recipe 1', 'This is a recipe!', 'https://cdn.pixabay.com/photo/2017/10/20/17/45/goulash-2872112_960_720.jpg')
+    new Recipe('Recipe 2', 'This is a recipe!', 'https://cdn.pixabay.com/photo/2017/10/20/17/45/goulash-2872112_960_720.jpg')
   ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeDeleted(name: string) {
+    const position: number = findIndex(this.recipes, { name });
+    this.recipes.splice(position, 1);
   }
 
 }
